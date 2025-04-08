81e2c4310e42e8f1870224994953443a5d735d1e
Brunorobson <bruno.assis@alu.unibalsas.edu.br>
2025-04-08 10:50:56 -0300

    fit: configuração banco de dados

---
+++ Database.php
@@ -0,0 +1,19 @@
+<?php
+class Database
+{
+    private $database;
+    public function __construct($config)
+    {
+        $connection = $config['driver'].':'.$config['database'];
+        $this->database = new PDO($connection);
+    }
+
+    public function query($query, $class = null, $params = []){
+        $prepare = $this->database->prepare($query);
+        if ($class) {
+            $prepare->setFetchMode(PDO::FETCH_CLASS, $class);
+        }
+        $prepare->execute($params);
+        return $prepare;
+    }
+}
