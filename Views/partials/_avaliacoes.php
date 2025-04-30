2add3ee92d1c6b7852b38aa219eec22aef30deac
Brunorobson <bruno.assis@alu.unibalsas.edu.br>
2025-04-30 18:19:38 -0300

    feat: avaliacoes

---
+++ Views/partials/_avaliacoes.php
@@ -0,0 +1,14 @@
+<div class="flex flex-col space-y-6">
+    <!-- Linha com título e pesquisa -->
+    <div class="flex justify-between items-center space-x-4 mb-4 mt-10">
+        <h2 class="text-xl font-rammetto text-slate-200">Avaliações</h2>
+
+        <div class="flex items-center gap-4">
+            <a href="/adicionar-avaliacao"
+                class="inline-flex items-center gap-2 px-4 h-10 rounded-md text-white bg-purple-900 hover:bg-purple-700 transition-colors">
+                <i class="fa-solid fa-plus"></i>
+                <span>Adicionar Avaliação</span>
+            </a>
+        </div>
+    </div>
+</div>
